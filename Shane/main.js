54b532a484a791a79bf4fa1c695fd82297b50e8e
Shane Blatt <shane.blatt@yahoo.com>
2015-01-26 10:22:10 -0500

    inital commit

---
+++ Shane/main.js
@@ -0,0 +1,19 @@
+console.log("This works");
+
+
+/**
+ * PART 1
+ *
+ * Write a JavaScript program that takes two numbers as
+ parameters and computes the sum of those two numbers
+ *
+ * For example, if the user input 3 and 6, then the answer would be: "9 (3+6)".
+ */
+
+
+
+
+var numbers = function (a, b, c, d, e, f, g, h, i){
+	return "1" + "2" + "3" + "4" + "fizz" + "6" + "7" + "8" + "9";
+};
+console.log(numbers(a, b, c, d, e, f, g, h, i));
